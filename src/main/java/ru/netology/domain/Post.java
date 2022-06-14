aa6924d01ff6bf8345282ef7656bdda35a9795ef
Aleksandr <artemyev86an@gmail.com>
2022-06-14 15:38:41 +0500

    addFormDate

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,14 @@
+package ru.netology.domain;
+
+import ru.netology.birthday.FormDate;
+
+public class Post {
+    public FormDate birthday;
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public String subscription;
+
+}
